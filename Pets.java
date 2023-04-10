3014baf15b9ced6fed6f841a5654817733801a1a
masuduzzaman niloy <98174422+masuduzzamanniloy901@users.noreply.github.com>
2023-04-10 23:30:55 +0600

    Add files via upload

---
+++ Pets.java
@@ -0,0 +1,36 @@
+package mid_ques;
+public class Pets {
+    String color = "White";
+    String habit = "Sitting";
+    int num = 5;
+
+    Pets(){
+        this.color = color;
+        this.habit = habit;
+    }
+    Pets(String color, String habit){
+        this.color = color;
+        this.habit = habit;
+    }
+    Pets(String color){
+        this.color = color;
+        this.habit = habit;
+    }
+
+    void printPets(){
+        System.out.println(color+" Cat is "+habit);
+    }
+
+    void updateInfo(String color){
+        this.color = color;
+    }
+    void updateInfo(int num){
+        this.num = num;
+        System.out.println("Brown Cat's weight is now " +num+ "kg");
+    }
+    void updateInfo(String color,String habit){
+        this.color = color;
+        this.habit = habit;
+    }
+
+}
